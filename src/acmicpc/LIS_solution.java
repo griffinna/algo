aa6a39857ee30ec4f4a322783de94ee05ba41147
griffinna <garam.dev9@gmail.com>
2020-04-01 20:10:57 +0900

    [CODE] 최장증가부분수열 (LIS)

---
+++ src/acmicpc/LIS_solution.java
@@ -0,0 +1,49 @@
+package acmicpc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class LIS_solution {
+
+    static int[] A = new int[313131];
+    static int[] RD = new int[313131];
+    static int N;
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        int rn = 0;
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int s = 0;
+            int e = rn - 1;
+            int k = rn;
+            System.out.printf("s : %s, e : %s, k : %s\n" ,s, e, k);
+            while(s <= e){
+                int m = (s + e) / 2;
+                if (RD[m] >= A[i]) {
+                    e = m - 1;
+                    k = m;
+                }else {
+                    s = m + 1;
+                }
+
+            }
+            RD[k] = A[i];
+            if(k == rn) rn++;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("rn : " + rn);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
