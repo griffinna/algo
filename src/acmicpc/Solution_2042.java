0111e217bdd164781f4eed91812785c64d9efb6c
griffinna <garam.dev9@gmail.com>
2020-05-04 22:19:04 +0900

    [ACMICPC][2042] Remove import

--- src/acmicpc/Solution_2042.java
+++ src/acmicpc/Solution_2042.java
@@ -2,7 +2,7 @@ package acmicpc;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 import java.util.StringTokenizer;
 
 /**
