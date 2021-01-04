049df5436c658136a6d103223e6223f4b911621f
griffinna <garam.dev9@gmail.com>
2021-01-04 22:59:06 +0900

    [ACMICPC][2606] 바이러스 (분류: BFS)
    Signed-off-by: Griffin <garam.dev9@gmail.com>

--- src/acmicpc/Solution_1260.java
+++ src/acmicpc/Solution_1260.java
@@ -6,29 +6,29 @@ import java.util.*;
 public class Solution_1260 {
 
     static int[][] arr;
-    static boolean[] visit;
-    static StringBuffer sb;
-    static int N;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
-        visit = new boolean[N + 1];
-        arr = new int[N + 1][N + 1];
-        sb = new StringBuffer();
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = 1;
-            arr[b][a] = 1;
-        }
-        dfs(V);
-        sb.append("\n");
-        Arrays.fill(visit, false);
-        bfs(V);
+        static boolean[] visit;
+        static StringBuffer sb;
+        static int N;
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            visit = new boolean[N + 1];
+            arr = new int[N + 1][N + 1];
+            sb = new StringBuffer();
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                arr[a][b] = 1;
+                arr[b][a] = 1;
+            }
+            dfs(V);
+            sb.append("\n");
+            Arrays.fill(visit, false);
+            bfs(V);
         System.out.println(sb.toString());
     }
 
