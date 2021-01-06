bf9fdd078f79cc0fd64f749c141e4f0654326ce3
griffinna <garam.dev9@gmail.com>
2021-01-06 20:41:54 +0900

    [ACMICPC][2003] 수들의 합2 (분류: 투포인터)
    Signed-off-by: Griffin <garam.dev9@gmail.com>

---
+++ src/acmicpc/Solution_2003.java
@@ -0,0 +1,37 @@
+package acmicpc;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_2003 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        long sum = 0;
+        int s = 0;
+        int e = 0;
+        long result = 0;
+
+        while (s < N) {
+            if (sum > M || e == N) {
+                sum -= arr[s];
+                s++;
+            } else {
+                sum += arr[e];
+                e++;
+            }
+            if (sum == M) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
