86db47a71753ee50e8fdfbb77c54eff2b25a265c
griffinna <garam.dev9@gmail.com>
2021-01-11 19:18:45 +0900

    [ACMICPC][2243] 사탕상자 (분류: SegmentTree) - 재풀이
    Signed-off-by: Griffin <garam.dev9@gmail.com>

--- src/acmicpc/SegmentTree/Solution_2243.java
+++ src/acmicpc/SegmentTree/Solution_2243.java
@@ -5,12 +5,17 @@ import java.util.*;
 
 public class Solution_2243 {
     static int N;   // 사탕상자에 손을 댄 횟수
+    static int MAX = 1000000;
     static long[] tree;
-
+    static int size;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        tree = new long[1000001 * 4];
+        size = 2;
+        while (size < MAX) {
+            size *= 2;
+        }
+        tree = new long[size * 2];
         int cmd, idx, cnt;
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
@@ -18,33 +23,34 @@ public class Solution_2243 {
             cmd = Integer.parseInt(st.nextToken());
             idx = Integer.parseInt(st.nextToken());
             if (cmd == 1) { // idx 번째 캔디 찾기
-                int n = getCandyIdx(1, 1, 1000001, idx);
+                int n = getCandyIdx(1, 1, size, idx);
                 System.out.println(n);
-                update(1, 1, 1000001, n, -1);
+                update(1, 1, size, n, -1);
             } else {
                 cnt = Integer.parseInt(st.nextToken());
-                update(1, 1, 1000001, idx, cnt);
+                update(1, 1, size, idx, cnt);
             }
         }
     }
 
-    static void update(int node, int start, int end, int idx, int val) {
-        if (idx < start || end < idx) return;
-        tree[node] += val;
-        if (start != end) {
-            int mid = (start + end) / 2;
-            update(node * 2, start, mid, idx, val);
-            update(node * 2 + 1, mid + 1, end, idx, val);
+    static long update(int node, int start, int end, int idx, int val) {
+        if(idx < start || end < idx) return tree[node];
+        if (start == end) {
+            return tree[node] += val;
         }
+
+        int mid = (start + end) / 2;
+        return tree[node] = update(node * 2, start, mid, idx, val) + update(node * 2 + 1, mid + 1, end, idx, val);
+
     }
 
     static int getCandyIdx(int node, int start, int end, int num) {    // num 번째 캔디 하나 꺼내기
-        if (start == end) {
+        if(start == end){
             return start;
-        } else {
+        }else{
             long leftCnt = tree[node * 2];
             int mid = (start + end) / 2;
-            if (leftCnt >= num) {
+            if (leftCnt >= num && node * 2 <= tree.length - 1) {
                 return getCandyIdx(node * 2, start, mid, num);
             }
             num -= leftCnt;
