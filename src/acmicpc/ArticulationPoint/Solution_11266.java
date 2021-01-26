13a19e025f15de0270adc695066ae554a6515b98
griffinna <garam.dev9@gmail.com>
2021-01-26 22:50:06 +0900

    Add Comment: [ACMICPC][11266] 단절점 (분류: Articulation Point)
    Signed-off-by: Griffin <garam.dev9@gmail.com>

--- src/acmicpc/ArticulationPoint/Solution_11266.java
+++ src/acmicpc/ArticulationPoint/Solution_11266.java
@@ -2,13 +2,18 @@ package acmicpc.ArticulationPoint;
 
 import java.io.*;
 import java.util.*;
-
+/**
+ * 단절점
+ * 조건 1. 정점 A가 루트 && 자식이 2개 이상이면 단절점이다.
+ * 조건 2. 정점 A의 자식노드들이 A를 거치지않고 A보다 빠른 방문번호를 가진다면 단절점이다.
+ *         (정점 A에서 자식노드들이 A를 거치지않고 정점 A 보다 빠른 방문번호를 가진 정점으로 갈 수 없다.)
+ * */
 public class Solution_11266 {
 
-    static ArrayList<Integer>[] adj;
-    static int[] index;
-    static boolean[] isJoint;
-    static int cnt;
+    static ArrayList<Integer>[] adj;    // 인접한 정점 정보
+    static int[] index;                 // 방문순번 정보
+    static boolean[] isJoint;           // 단절점여부
+    static int cnt;                     // 방문번호 (1부터 시작)
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,6 +34,7 @@ public class Solution_11266 {
         }
         cnt = 1;
         for (int i = 1; i <= V; i++) {
+            // 아직 방문하지 않은 정점을 루트로 가정하고 dfs 수행
             if(index[i] == 0) dfs(i, true);
         }
         StringBuffer sb = new StringBuffer();
@@ -44,21 +50,23 @@ public class Solution_11266 {
     }
 
     private static int dfs(int idx, boolean isRoot) {
-        index[idx] = cnt++;
-        int min = index[idx];
-        int child = 0;
-        if (adj[idx] != null) {
-            for (int next: adj[idx]) {
-                if (index[next] == 0) {
-                    child++;
-                    int low = dfs(next, false);
-                    min = Math.min(min, low);
+        index[idx] = cnt++;         // idx 번째 정점의 방문순번을 저장하고 cnt 를 1 증가시킨다
+        int min = index[idx];       // 현재 정점의 방문순번 최소 (return 할 값)
+        int child = 0;              // 자식노드 카운트 할 변수
+        if (adj[idx] != null) {     // 인접한 정점이 있다면
+            for (int next: adj[idx]) {      // 인접한 정점들 중에서
+                if (index[next] == 0) {     // next 정점을 아직 방문하지 않았으면
+                    child++;                // idx 번째 정점의 자식이다.
+                    int low = dfs(next, false);     // next 정점을 dfs 탐색한 후 방문순번 최소값을 찾는다.
+                    min = Math.min(min, low);               // idx 번째 정점의 방문순번 최소값과 비교한다.
+                    // idx 정점이 루트정점이고, next 정점의 방문순번최소값이 더 작으면 idx 정점은 단절점이다.
                     if (!isRoot && low >= index[idx]) isJoint[idx] = true;
-                } else {
-                    min = Math.min(min, index[next]);
+                } else {                // next 정점을 이미 방문했으면
+                    min = Math.min(min, index[next]);       // idx 번째 정점의 방문순번 최소값과 next 번째 정점의 방문순번을 비교한다.
                 }
             }
         }
+        // 정점 루트이고, 자식이 2개 이상이라면 단절점이다.
         if (isRoot && child >= 2) {
             isJoint[idx] = true;
         }
